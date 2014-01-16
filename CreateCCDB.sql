078f9581479332c29c3a4a6f009e6b93a161384e
Marla Crews <deafsongbird@gmail.com>
2014-01-16 13:07:25 -0500

    More changes

--- CreateCCDB.sql
+++ CreateCCDB.sql
@@ -21,7 +21,7 @@ create table customers
 (cust_id int not null PRIMARY KEY,
 first_name varchar(15) not null,
 last_name varchar(30) not null,
-street_address varchar(50) not null,
+street_address varchar(50) null,
 city varchar(25) not null,
 state_abbrev char(2) not null,
 zip_code char(5) not null,
@@ -52,7 +52,7 @@ last_name varchar(30) not null,
 city varchar(25) not null,
 state_abbrev char(2) not null,
 email varchar(100) null,
-comment_area text not null);
+comment_area varchar(250) not null);
 
 create table tax_rate
 (tax_id char(3) not null PRIMARY KEY,
@@ -60,17 +60,16 @@ tax_label varchar(25) not null,
 rates decimal not null);
 
 create table site_admins
-(admin_id int not null PRIMARY KEY,
+(admin_name varchar not null PRIMARY KEY,
 username varchar (20) not null FOREIGN KEY references login(username),
 password varchar(15) not null FOREIGN KEY references login(password)
-admin_name varchar(30) not null,
 active bit not null);
 
 create table products
 (item_number int not null PRIMARY KEY,
 item_name varchar(100) not null,
 item_type varchar(100) not null,
-item_description varchar(200) not null,
+item_description varchar(250) not null,
 more_info varchar(100) not null,
 inventory int not null FOREIGN KEY references inventory(in_stock));
 
