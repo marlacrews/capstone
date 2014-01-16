ade3bbbc74d64ff17e22adc482fb000aa04b5765
Marla Crews <deafsongbird@gmail.com>
2014-01-16 08:49:20 -0500

    added more tables to database for products

--- CreateCCDB.sql
+++ CreateCCDB.sql
@@ -21,12 +21,13 @@ create table customers
 (cust_id int not null PRIMARY KEY,
 first_name varchar(15) not null,
 last_name varchar(30) not null,
-street_address varchar(50) null,
-city varchar(25) null,
-state_abbrev char(2) null,
-zip_code char(5) null,
+street_address varchar(50) not null,
+city varchar(25) not null,
+state_abbrev char(2) not null,
+zip_code char(5) not null,
 birthday date null,
-email varchar(100) not null);
+email varchar(100) not null,
+username varchar(20) FOREIGN KEY references login(username));
 
 create table login
 (username varchar(20) not null PRIMARY KEY,
@@ -41,7 +42,8 @@ emp_id int not null FOREIGN KEY References employee(emp_id),
 hire_date date not null,
 end_date date null,
 reason varchar(200) null,
-rehireable bit null);
+rehireable bit null,
+username varchar(20) FOREIGN KEY references login(username));
 
 create table guestbook
 (id_num int not null PRIMARY KEY,
@@ -63,3 +65,17 @@ username varchar (20) not null FOREIGN KEY references login(username),
 password varchar(15) not null FOREIGN KEY references login(password)
 admin_name varchar(30) not null,
 active bit not null);
+
+create table products
+(item_number int not null PRIMARY KEY,
+item_name varchar(100) not null,
+item_type varchar(100) not null,
+item_description varchar(200) not null,
+more_info varchar(100) not null,
+inventory int not null FOREIGN KEY references inventory(in_stock));
+
+create table inventory
+(item_number int FOREIGN KEY references product(item_number),
+in_stock int,
+qty_limited bit not null,
+reorder bit not null,);
