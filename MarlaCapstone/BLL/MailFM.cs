899b57c37ad4d765506ac6e0db8e5aa3039567ae
unknown <admin2@LAPTOP-135.onshoretechnology.local>
2014-01-18 21:11:38 -0500

    Trying to make changes

--- MarlaCapstone/BLL/MailFM.cs
+++ MarlaCapstone/BLL/MailFM.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace BLL
 {
     public class MailFM
     {
+        [Display(Name = "From:")]
         public string From { get; set; }
+
+        [Display(Name = "To:")]
         public string To { get; set; }
+
+        [Display(Name = "Subject")]
         public string Subject { get; set; }
+
+        [Display(Name = "Body")]
         public string Body { get; set; }
     }
+
+
+    //{
+    //    public string From { get; set; }
+    //    public string To { get; set; }
+    //    public string Subject { get; set; }
+    //    public string Body { get; set; }
+    //}
 }
---
+++ MarlaCapstone/BLL/MailServices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+using DAL;
+	{
+		 
+	}
+
+namespace BLL
+{
+    public class MailServices
+    {
+    }
+}
