e66a45740cc29a6ce7612ab7f7ad2f48e268e457
unknown <admin2@LAPTOP-135.onshoretechnology.local>
2014-01-20 09:38:33 -0500

    Added myself to the userlogin and admin database.

---
+++ addmyself.sql
@@ -0,0 +1,5 @@
+INSERT INTO userlogin (username, user_password, user_firstname, user_last_name, active)
+VALUES ('marlacrews', '6Hen2107', 'Marla', 'Crews', 1 );
+
+INSERT INTO site_admins (userID, active)
+VALUES (1, 1);
