c7cef72815bb2577b801d94aaca8f6b255e23f1a
unknown <admin2@LAPTOP-135.onshoretechnology.local>
2014-01-21 02:27:33 -0500

    Following tutorial to try to fix and to create project.

---
+++ MarlaCapstone/MarlaCapstone/Models/Guestbook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MarlaCapstone.Models
+{
+    public class Guestbook
+    {
+        public int entry_num { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string city { get; set; }
+        public string state_abbrev { get; set; }
+        public string email { get; set; }
+        public string comment_area { get; set; }
+    }
+
+    public class GuestbookDbContext : ApplicationDbContext
+    {
+        public DbSet<Guestbook> Guestbook { get; set; }
+    }
+}
---
+++ MarlaCapstone/SQLQuery1.sql
@@ -0,0 +1,8 @@
+﻿create table guestbook
+(entry_num int not null PRIMARY KEY IDENTITY,
+first_name varchar(15) not null,
+last_name varchar(30) not null,
+city varchar(25) not null,
+state_abbrev char(2) not null,
+email varchar(100) null,
+comment_area varchar(250) not null)
