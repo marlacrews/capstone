71fae223bc9a9008b2a8dcefa808d16ca3471d50
unknown <admin2@LAPTOP-135.onshoretechnology.local>
2014-01-21 13:09:26 -0500

    Added picture of the store

---
+++ MarlaCapstone/MarlaCapstone/Controllers/GuestBookController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MarlaCapstone.Controllers
+{
+    public class GuestBookController : Controller
+    {
+        //
+        // GET: /GuestBook/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
