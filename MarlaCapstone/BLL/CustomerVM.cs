ad3e238b4feacc8a95048cb202b0e9e8834a6583
unknown <admin2@LAPTOP-135.onshoretechnology.local>
2014-01-21 09:22:27 -0500

    Deleting classes that were made incorrectly.

--- MarlaCapstone/BLL/CustomerVM.cs
+++ MarlaCapstone/BLL/CustomerVM.cs
@@ -3,16 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Net.Mail;
-using System.Net;
-using DAL;
-	{
-		 
-	}
 
 namespace BLL
 {
-    public class MailServices
+    class CustomerVM
     {
     }
 }
